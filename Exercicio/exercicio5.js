00990f72445875c1396d39f3113d7f4a54f94dc6
gustavo <gu35km@hotmail.com>
2020-01-17 15:06:38 -0300

    fis os exercicios 4 e 5

---
+++ Exercicio/exercicio5.js
@@ -0,0 +1,9 @@
+let numero = prompt("Numero interio")
+
+numero = parseFloat(numero)
+
+console.log(numero*numero)
+
+console.log("O numero elevado ao quadrado é...  " +(numero*numero))
+
+let total = numero * numero
