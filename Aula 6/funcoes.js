02a294e67c0202b011356e486e7b9ab92e6a4068
Gustavo Gomes <semogavlis08@gmail.com>
2020-01-30 22:32:01 -0300

    Aula 6 feita hoje e o exercicio 22 feito hoje ta,bem

---
+++ Aula 6/funcoes.js
@@ -0,0 +1,9 @@
+
+function Mudarsite(){
+
+    document.getElementById("Titulo").innerHTML = "Meu Site";
+
+    //document.getElementById("Login").innerHTML += "Tedious";
+
+    document.getElementById("Login").innerHTML += "<a href='https://www.wattpad.com/?locale=pt_PT'> Link</a>";
+    }
--- Exercicio/exercicio22.js
+++ Exercicio/exercicio22.js
@@ -1,8 +1,20 @@
-let numero
+let numero, valor, total=0, media;
 
 numero=prompt("Quantos numeros de serie você quer ?")
-document.write(numero)
 
+while(numero<=0){
+    alert("numero menor ou igual a zero")
+    numero=prompt("Quantos numeros de serie voce quer?")
+}
+
+for(let i=1; i<=numero; i++){
+    valor=prompt("Digite um numero de serie")
+    total=parseInt(valor)+numero
+    
+}
 if(numero<=0){
-    document.write("Não pode numeros negativos ou igual a zero")
-}else
+    numero=prompt("Um numero não pode ser menor ou igual a zero! Digite um novo numero de serie")
+}
+
+media= total/numero
+document.write(" Amedia é: " + media)
